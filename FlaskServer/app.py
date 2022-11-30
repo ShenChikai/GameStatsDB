41a56ad7ffab2d8db1d0b996044e44fa1f7d6340
Denny Shen <shenchikai@outlook.com>
2022-11-30 16:48:09 -0500

    bg position now fit mobile

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -35,4 +35,8 @@ def about():
 
 # Main
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(host="0.0.0.0", port="5000")
+
+# Test Main
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000)

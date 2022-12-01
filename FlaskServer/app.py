6b2b0fe0b1d23d69789068e9a6db69d047105950
Denny Shen <shenchikai@outlook.com>
2022-11-30 23:49:51 -0500

    test with apis

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
+import requests
 # from views import views
 
 # Flask Obj Creation
@@ -23,20 +24,35 @@ def table1():
         print(args['Genre'])
         return redirect(url_for("test", name=args))
     else:
+       
         return render_template("table1.html")
 
 @app.route("/table2")
 def table2():
-    return render_template("table2.html")
+    # testing
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1"
+    response = requests.get(url)
+    rawData = response.json()
+    data = list(map(lambda x: {
+        'Name': x['name'],
+        'Price': x['current_price'],
+        '24High': x['high_24h'],
+        '24Low': x['low_24h'],
+        'MC': str(int(x['market_cap']) // 1_000_000) + 'M',
+    }, rawData))
+    print(data)
+    return render_template("table2.html", data = data)
+
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 # Main
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port="5000")
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port="5000")
 
 # Test Main
-# if __name__ == '__main__':
-#     app.run(debug=True, port=8000)
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
--- FlaskServer/static/table.js
+++ FlaskServer/static/table.js
@@ -1,69 +1,44 @@
-data = [
-    {
-        name: "name1",
-        age: 11,
-        gender: "male"
-    },
-    {
-        name: "name2",
-        age: 27,
-        gender: "male"
-    },
-    {
-        name: "name3",
-        age: 36,
-        gender: "female"
-    },
-    {
-        name: "name4",
-        age: 42,
-        gender: "male"
-    },
-    {
-        name: "name5",
-        age: 55,
-        gender: "female"
-    },
-]
+// General Create Table Function
+function createTable(data, target) {
 
+    var table = document.createElement('table');
+    table.classList.add("table");    // from bootstrap 
+    table.classList.add("table-hover");
+    table.classList.add("table-dark"); 
 
-var table = document.createElement('table');
-table.classList.add("table");    // from bootstrap 
-table.classList.add("table-hover");
-table.classList.add("table-dark"); 
-
-// header
-var thead = document.createElement('thead');
-var row = document.createElement('tr');
-var cell = document.createElement('th');    // Index Header
-cell.scope = "col";
-cell.textContent = "Index";
-row.appendChild(cell);
-for (const prop in data[0]) {
-    var cell = document.createElement('th');
+    // header
+    var thead = document.createElement('thead');
+    var row = document.createElement('tr');
+    var cell = document.createElement('th');    // Index Header
     cell.scope = "col";
-    cell.textContent = prop;
-    row.appendChild(cell);
-}
-thead.appendChild(row);
-table.appendChild(thead);
-
-// data
-var tbody = document.createElement('tbody');
-for (var i = 0; i < data.length; i++) {
-    row = document.createElement('tr');
-    var cell = document.createElement('th');    // Row Num
-    cell.scope = "row";
-    cell.textContent = i;
+    cell.textContent = "Index";
     row.appendChild(cell);
     for (const prop in data[0]) {
-        var cell = document.createElement('td');
-        cell.textContent = data[i][prop];
+        var cell = document.createElement('th');
+        cell.scope = "col";
+        cell.textContent = prop;
         row.appendChild(cell);
     }
-    tbody.appendChild(row);
-}
-table.appendChild(tbody);
+    thead.appendChild(row);
+    table.appendChild(thead);
 
-// append to div
-document.getElementById('tableDiv').appendChild(table);
+    // data
+    var tbody = document.createElement('tbody');
+    for (var i = 0; i < data.length; i++) {
+        row = document.createElement('tr');
+        var cell = document.createElement('th');    // Row Num
+        cell.scope = "row";
+        cell.textContent = i;
+        row.appendChild(cell);
+        for (const prop in data[0]) {
+            var cell = document.createElement('td');
+            cell.textContent = data[i][prop];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+
+    // append to div
+    document.getElementById(target).appendChild(table);
+}

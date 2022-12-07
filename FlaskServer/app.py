e43baa44be7859201d8ce01b0475b3a54c7c046a
Denny Shen <shenchikai@outlook.com>
2022-12-07 18:24:36 -0500

    Switch to production mode
    turn off the developer debug mode

--- FlaskServer/app.py
+++ FlaskServer/app.py
@@ -83,9 +83,9 @@ def about():
     return render_template("about.html")
 
 # Main
-# if __name__ == '__main__':
-#     app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
 
 # Test Main
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+# if __name__ == '__main__':
+#     app.run(debug=True, port=8000)
